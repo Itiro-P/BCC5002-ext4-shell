c301b31b6dfa8b17e8830bb6f13b40585520bb8b
Pedro N <itiropedro@gmail.com>
2026-05-28 17:20:27 -0300

    Update Image.hpp

--- include/Ext4/Image.hpp
+++ include/Ext4/Image.hpp
@@ -11,8 +11,11 @@ namespace Ext4 {
         std::fstream image_file;
     public:
         explicit Image(const std::string& image_path);
+        // Desabilita a cópia para evitar problemas de gerenciamento de recursos.
         Image(const Image&) = delete;
         Image& operator=(const Image&) = delete;
+        
+        // Permite a movimentação para facilitar o gerenciamento de recursos.
         Image(Image&&) = default;
         Image& operator=(Image&&) = default;
         ~Image() { if(image_file.is_open()) image_file.close(); }
