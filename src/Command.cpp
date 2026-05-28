25084019d993f03ffcd39d50f766d6394855db67
Pedro N <itiropedro@gmail.com>
2026-05-27 22:01:11 -0300

    Update Command.cpp

--- src/Command.cpp
+++ src/Command.cpp
@@ -1,4 +1,5 @@
 #include "../include/Command.hpp"
+#include <iostream>
 
 std::vector<std::pair<std::string, std::string>> Command::command_info{
     {"info",                            "Exibe informações da imagem e do sistema de arquivos."},
@@ -14,9 +15,9 @@ std::vector<std::pair<std::string, std::string>> Command::command_info{
     {"mkdir <diretório>",               "Cria um novo diretório."},
     {"rm <arquivo>",                    "Remove um arquivo."},
     {"rmdir <diretório>",               "Remove um diretório vazio."},
-    {"rename <antigo> <novo>",          "Renomeia um arquivo ou diretório."},
+    {"rename <arquivo> <novo nome>",    "Renomeia um arquivo ou diretório."},
     {"exit",                            "Encerra o shell."},
-    {"clear",                           "Limpa a tela do terminal."}
+    {"clear",                           "Limpa a tela do shell."}
 };
 
 short Command::info() {
@@ -28,54 +29,139 @@ short Command::info() {
 
 short Command::cat(const std::vector<std::string>& args) {
     const std::string file_path = args.size() > 1 ? args[1] : "";
+
+    if(file_path.empty()) {
+        std::println(std::cerr, "Uso: cat <arquivo>");
+        return 1;
+    }
+
     return 0;
 }
 
 short Command::attr(const std::vector<std::string>& args) {
+    const std::string target_path = args.size() > 1 ? args[1] : "";
+
+    if(target_path.empty()) {
+        std::println(std::cerr, "Uso: attr <arquivo/diretório>");
+        return 1;
+    }
+
     return 0;
 }
 
 short Command::cd(const std::vector<std::string>& args) {
+    const std::string target_path = args.size() > 1 ? args[1] : "";
+
+    if(target_path.empty()) {
+        std::println(std::cerr, "Uso: cd <diretório>");
+        return 1;
+    }
+
     return 0;
 }
 
 short Command::ls(const std::vector<std::string>& args) {
+    const std::string target_path = args.size() > 1 ? args[1] : "";
+
+    if(target_path.empty()) {
+        std::println(std::cerr, "Uso: ls <diretório>");
+        return 1;
+    }
+
     return 0;
 }
 
 short Command::test_inode(const std::vector<std::string>& args) {
+    const std::string inode_str = args.size() > 1 ? args[1] : "";
+
+    if(inode_str.empty()) {
+        std::println(std::cerr, "Uso: test_inode <inode>");
+        return 1;
+    }
+
     return 0;
 }
 
 short Command::test_block(const std::vector<std::string>& args) {
+    const std::string block_str = args.size() > 1 ? args[1] : "";
+
+    if(block_str.empty()) {
+        std::println(std::cerr, "Uso: test_block <bloco>");
+        return 1;
+    }
+
     return 0;
 }
 
 short Command::cp(const std::vector<std::string>& args) {
+    const std::string source_path = args.size() > 1 ? args[1] : "";
+    const std::string dest_path = args.size() > 2 ? args[2] : "";
+
+    if(source_path.empty() || dest_path.empty()) {
+        std::println(std::cerr, "Uso: cp <arquivo1> <arquivo2>");
+        return 1;
+    }
+
     return 0;
 }
 
 short Command::pwd() {
+
     return 0;
 }
 
 short Command::touch(const std::vector<std::string>& args) {
+    const std::string file_path = args.size() > 1 ? args[1] : "";
+
+    if(file_path.empty()) {
+        std::println(std::cerr, "Uso: touch <arquivo>");
+        return 1;
+    }
+
     return 0;
 }
 
 short Command::mkdir(const std::vector<std::string>& args) {
+    const std::string dir_path = args.size() > 1 ? args[1] : "";
+
+    if(dir_path.empty()) {
+        std::println(std::cerr, "Uso: mkdir <diretório>");
+        return 1;
+    }
+
     return 0;
 }
 
 short Command::rm(const std::vector<std::string>& args) {
+    const std::string file_path = args.size() > 1 ? args[1] : "";
+
+    if(file_path.empty()) {
+        std::println(std::cerr, "Uso: rm <arquivo>");
+        return 1;
+    }
+
     return 0;
 }
 
 short Command::rmdir(const std::vector<std::string>& args) {
+    const std::string dir_path = args.size() > 1 ? args[1] : "";
+
+    if(dir_path.empty()) {
+        std::println(std::cerr, "Uso: rmdir <diretório>");
+        return 1;
+    }
+
     return 0;
 }
 
 short Command::rename(const std::vector<std::string>& args) {
+    const std::string file = args.size() > 1 ? args[1] : "";
+    const std::string new_file_name = args.size() > 2 ? args[2] : "";
+
+    if(file.empty() || new_file_name.empty()) {
+        std::println(std::cerr, "Uso: rename <arquivo> <novo nome do arquivo>");
+        return 1;
+    }
     return 0;
 }
 
